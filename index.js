188783f9d0e7c360e3085f0215f1dc79015b6ddb
Jankowiec <ghjankowiec@gmail.com>
2024-05-01 15:50:17 +0200

    ADD: Room isOccupied method and testing cases

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ class Room {
         this.discount = discount;
     };
 
-    isOccupied(date) {
+    setBookings(...args) {
+        args.forEach(arg => this.bookings.push(arg))
+    }
 
+    isOccupied(date) {
+        let occupied = false;
+        if(!date || date === '') {
+            throw new Error('Error!! Missing Date!!');
+        } else if(Number(date)) {
+            throw new Error('Error!! Wrong date format!!');
+        }
+        for(let i = 0; i < this.bookings.length; i++) {
+            let range = rangeDates(this.bookings[i].checkin, this.bookings[i].checkout)
+            if(range.includes(date)) {
+                occupied = true;
+            }
+        }
+        return occupied;
+        
     };
     
     percentageOccupancy(startDate, endDate) {
@@ -42,4 +59,24 @@ class Booking {
 
 };
 
+function rangeDates(start, end) {
+    let startDate = new Date(start);
+    let endDate = new Date(end)
+    let range = [];
+    for(let day = startDate; day < endDate; day.setDate(day.getDate()+1)) {
+        range.push(day.toISOString().slice(0, 10));
+    }
+    return range;
+}
+
 module.exports = { Room, Booking };
+
+
+/* let rango = rangeDates('2024-04-15', '2024-05-01');
+console.log(rango); */
+
+/* const room = new Room({name: 'una room', price: 200, discount: 25});
+const booking = new Booking({name: 'unName', email: 'unmail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10})
+const booking2 = new Booking({name: 'otroName', email: 'otromail@mail.com', checkin: '2024-04-15', checkout: '2024-05-01', discount: 10})
+room.setBookings(booking, booking2);
+console.log(room); */
--- index.test.js
+++ index.test.js
@@ -1,1 +1,154 @@
 const { Room, Booking } = require('./index');
+
+describe('Room - Checking isOccupied method', () => {
+
+  test('Date parameter missing', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-04-30', checkout: '2024-05-06', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(() => {room.isOccupied()}).toThrow(new Error('Error!! Missing Date!!'));
+  });
+  
+  test('Wrong date format', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-04-30', checkout: '2024-05-06', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(() => {room.isOccupied(20240501)}).toThrow(new Error('Error!! Wrong date format!!'));
+  });
+
+  test('room is occupied, first booking first day', () => {
+
+      const room = new Room({name: 'room1', price: 200, discount: 25});
+
+      const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+      const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+      const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+      const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+      const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-04-30', checkout: '2024-05-06', discount: 10, room: 'room1'});
+
+      room.setBookings(booking, booking2, booking3, booking4, booking5);
+      
+      expect(room.isOccupied('2024-05-01')).toBeTruthy();
+  });
+
+  test('room is occupied, last booking first day', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-04-30', checkout: '2024-05-06', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(room.isOccupied('2024-04-30')).toBeTruthy();
+  });
+
+  test('room is occupied, first and last booking before checkout', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-04-30', checkout: '2024-05-06', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(room.isOccupied('2024-05-05')).toBeTruthy();
+  });
+
+  test('room is occupied, middle booking first day', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-04-30', checkout: '2024-05-06', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(room.isOccupied('2024-03-10')).toBeTruthy();
+  });
+    
+  test('room is occupied, middle booking middle day', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-04-30', checkout: '2024-05-06', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(room.isOccupied('2024-05-23')).toBeTruthy();
+  });
+
+  test('room is not occupied, first booking checkout day', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-06-08', checkout: '2024-06-15', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(room.isOccupied('2024-05-06')).toBeFalsy();
+  });
+
+  test('room is not occupied, last booking checkout day', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-06-08', checkout: '2024-06-15', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(room.isOccupied('2024-06-15')).toBeFalsy();
+  });
+
+  test('room is not occupied, middle booking checkout day', () => {
+
+    const room = new Room({name: 'room1', price: 200, discount: 25});
+
+    const booking = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-05-01', checkout: '2024-05-06', discount: 10, room: 'room1'});    
+    const booking2 = new Booking({name: 'name2', email: 'mail@mail.com', checkin: '2024-03-04', checkout: '2024-03-10', discount: 10, room: 'room1'});
+    const booking3 = new Booking({name: 'name3', email: 'mail@mail.com', checkin: '2024-03-10', checkout: '2024-03-11', discount: 10, room: 'room1'});
+    const booking4 = new Booking({name: 'name4', email: 'mail@mail.com', checkin: '2024-05-07', checkout: '2024-06-07', discount: 10, room: 'room1'});
+    const booking5 = new Booking({name: 'name1', email: 'mail@mail.com', checkin: '2024-06-08', checkout: '2024-06-15', discount: 10, room: 'room1'});
+
+    room.setBookings(booking, booking2, booking3, booking4, booking5);
+    
+    expect(room.isOccupied('2024-03-11')).toBeFalsy();
+  });
+});
